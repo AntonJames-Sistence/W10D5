422d5bae9da6318cefc05d9aa4aaa3dff121b898
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-05-12 16:53:21 -0400

    Phase 1

--- art-museum/src/components/GalleryNavigation/index.js
+++ art-museum/src/components/GalleryNavigation/index.js
@@ -1,9 +1,21 @@
+import { NavLink }from 'react-router-dom';
 
-
-const GalleryNavigation = (galleries) => {
-    debugger
+const GalleryNavigation = ({galleries}) => {
+    
+    const links = galleries.map((ele) => {
+        // debugger
+        // console.log(ele.galleryid);
+        return <NavLink key={ele.galleryid} to={`/galleries/${ele.galleryid}`}> {ele.name} </NavLink>
+    });
+    // console.log(galleries)
+    // debugger
     return (
-        <h1>{galleries.galleries[0].floor}</h1>
+        <nav className='test'>
+            <h1>Galleries</h1>
+            {links}
+            
+            {/* <NavLink to="/" activeStyle={{ color: 'red' }}>gal</NavLink> */}
+        </nav>
     )
 }
 
--- art-museum/src/index.js
+++ art-museum/src/index.js
@@ -4,11 +4,7 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 
-const Component = (props) => {
-  return (
-    <h1>Hello from Root</h1>
-  )
-}
+
 
 ReactDOM.render(
   <React.StrictMode>
